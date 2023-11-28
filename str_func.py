49fee89c95d23fe59242935620b5947a65d90b43
Maksim Bolotin <vk07122012@gmail.com>
2023-11-29 00:14:10 +0300

    new function available

--- str_func.py
+++ str_func.py
@@ -3,3 +3,10 @@ def str_func(text: str) -> str:
     возвращает строку со всеми заглавными буквами.
     """
     return text.upper()
+
+
+def cap_func(text: str) -> str:
+    """
+    Возвращает строку с заглавными первыми буквами каждого слова.
+    """
+    return text.title()
