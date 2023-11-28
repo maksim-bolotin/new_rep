f0c20c2ee1bc77e88923b06e17bf863f3fdc4322
maksim-bolotin <150610016+maksim-bolotin@users.noreply.github.com>
2023-11-29 00:29:29 +0300

    develop (#2)
    * Update README.md
    
    * added gitignore file
    
    * Feature/task2  (#1)
    
    * created str_func
    
    * added gitignore
    
    * Update README.md
    
    * Update str_func.py
    
    * new function available

---
+++ str_func.py
@@ -0,0 +1,12 @@
+def str_func(text: str) -> str:
+    """
+    Возвращает строку со всеми заглавными буквами.
+    """
+    return text.upper()
+
+
+def cap_func(text: str) -> str:
+    """
+    Возвращает строку с заглавными первыми буквами каждого слова.
+    """
+    return text.title()
